58942427ed7f22e1fa58c73d5ec63c90071ea012
Svetlana Titova <oracle9i3@hotmail.com>
2016-08-17 17:13:59 +0300

    Style and images.

---
+++ src/main/java/lv/javaguru/java2/database/CustomerDAO.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java2.database;
+
+import lv.javaguru.java2.domain.Customer;
+
+import java.util.List;
+
+/**
+ * Created by Svetlana Titova on 17.08.2016.
+ */
+public interface CustomerDAO {
+
+    List<Customer> getAll() throws DBException;
+}
