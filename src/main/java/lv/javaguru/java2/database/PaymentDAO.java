d987cbb382268694b1da6714c3fc15fcca672fa2
Svetlana Titova <oracle9i3@hotmail.com>
2016-08-18 13:37:41 +0300

    Payment impl ( initial )

--- src/main/java/lv/javaguru/java2/database/PaymentDAO.java
+++ src/main/java/lv/javaguru/java2/database/PaymentDAO.java
@@ -8,6 +8,6 @@ import java.util.List;
  * Created by Svetlana Titova on 18.08.2016.
  */
 public interface PaymentDAO {
-    Payment getPaymentById(int id) throws DBException;
-    List<Payment> getAllPayments() throws DBException;
+    List<Payment> getAllPaymentByCustId(int id) throws DBException;
+
 }
